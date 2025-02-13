125f96587a623f47a90c81edae42bcbff1dbc648
Navdeep Singh <navdeepbrar200422@gmail.com>
2025-02-13 23:43:15 +0530

    adding password hashing

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
-import User from '../models/user.models.js'
+import User from '../models/user.models.js';
+import bcrypt from 'bcryptjs';
 
 export const signup = async (req,res)=>{
     const {username,email,password} = req.body;
@@ -8,14 +9,18 @@ export const signup = async (req,res)=>{
         if(user) return res.status(400).json({message: "User with this Email already exists"});
 
         const isUsername = await User.findOne({username});
-        if(isUsername) return res.status(400).json({message: "Username is already taken"})
+        if(isUsername) return res.status(400).json({message: "Username is already taken"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
 
         const newUser = new User({
             name: username,
             username,
             email,
-            password
+            password: hashedPassword
         });
+
 
         if(newUser){
             await newUser.save();
@@ -26,7 +31,7 @@ export const signup = async (req,res)=>{
                 username: newUser.username,
                 email: newUser.email,
                 pic: newUser.pic,
-            })
+            });
         }else{
             res.status(400).json({message: "Invalid user data"});
         }
